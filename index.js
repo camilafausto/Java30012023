baae1897a5c0ccff9e468f46ea24419d5a245982
camila <camila.vasconcelos.cf@gmail.com>
2023-01-30 22:39:04 -0300

    Java30012023

---
+++ index.js
@@ -0,0 +1,9 @@
+var nome= prompt("Qual seu nome?");
+
+alert("Seja bem vinda" + nome);
+
+var h1 = document.getElementById("Seja bem vinda Camila");
+
+h1.innerText = "Seja bem vindo" + Camila;
+
+h1.style.color= "violet";
